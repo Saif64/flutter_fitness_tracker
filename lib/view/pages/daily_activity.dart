3a2b585a0417f63ff0bfc2e92c29b43d082c867c
saif64s <my@wo.rk>
2024-08-17 12:18:46 +0600

    initial commit

---
+++ lib/view/pages/daily_activity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DailyActivity extends StatefulWidget {
+  const DailyActivity({Key? key}) : super(key: key);
+
+  @override
+  _DailyActivityState createState() => _DailyActivityState();
+}
+
+class _DailyActivityState extends State<DailyActivity> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/view/pages/goal_tracking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoalTracking extends StatefulWidget {
+  const GoalTracking({Key? key}) : super(key: key);
+
+  @override
+  _GoalTrackingState createState() => _GoalTrackingState();
+}
+
+class _GoalTrackingState extends State<GoalTracking> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
